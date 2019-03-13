f3ff11b5079f136ad95f2a2bbdd59831c01c5a9e
apple <wow@github.com>
2019-03-13 15:18:57 +0800

    init

---
+++ src/main/java/com/github/fish56/HelloSpringBoot/controller/Hello.java
@@ -0,0 +1,14 @@
+package com.github.fish56.HelloSpringBoot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class Hello {
+    @GetMapping
+    public String sayHello(){
+        return "Hello world";
+    }
+}
